db87cfa7567c8147fcfae11d3ed128a079448e86
romaric roussel <romaric.roussel@outlook.fr>
2018-11-16 17:33:16 +0100

    affichage liste pokemon avec nouvelle api

--- Pokecard/app/src/main/java/com/example/lpiem/pokecard/API.java
+++ Pokecard/app/src/main/java/com/example/lpiem/pokecard/API.java
@@ -5,9 +5,9 @@ import java.util.List;
 import retrofit2.*;
 import retrofit2.http.*;
 
-public interface API {
+interface API {
 
-    String url= "http://10.0.2.2/Pokecard/api/Pokemon/";
+    var url = "http://10.0.2.2/Pokecard/api/Pokemon/";
     @GET("read.php")
     Call <Example>getData();
 
