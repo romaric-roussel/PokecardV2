abf079f69b5097c4ca7280d66baf75a74f8ea75b
soso <sof.benzaied@gmail.com>
2019-04-05 11:35:34 +0200

    access to exchange screen and friend screen

---
+++ Pokecard/app/src/main/java/com/example/lpiem/pokecard/fragment/FragmentFriend.kt
@@ -0,0 +1,16 @@
+package com.example.lpiem.pokecard.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.lpiem.pokecard.R
+
+class FragmentFriend : BaseFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_friend, container, false)
+    }
+}
